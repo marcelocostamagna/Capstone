6a55c0197dd68978b54fdf36ec1aaf94f3b49ecb
marcelocostamagna <marcelotto696@yahoo.com.ar>
2017-02-03 12:48:01 -0300

    n

--- EDA-ModelSelection.R
+++ EDA-ModelSelection.R
@@ -33,16 +33,36 @@ ames_train %>%
   theme(axis.text.x =   element_text(angle=45,  size=10)) +
    scale_y_continuous( labels = comma)
 
-ames_train %>% 
+nbh_dist<- ames_train %>% 
   group_by(Neighborhood) %>% 
-  summarise(max=max(price), min=min(price),average=mean(price),median=median(price))
-  
+  summarise(max=max(price), min=min(price),average=mean(price),median=median(price),std=sd(price))
+nbh_dist<- data.frame(nbh_dist)
+
+nbh_dist %>% select(Neighborhood,max)  %>% top_n(1)
+nbh_dist %>% select(Neighborhood,min)  %>% top_n(-1) 
+nbh_dist %>% select(Neighborhood,average)  %>% top_n(1)
+nbh_dist %>% select(Neighborhood,std)  %>% top_n(1)
+
+
 
 #q2
 
+#Q3
+summary(ames_train)
 
 
+tmp<-data.frame(apply(ames_train, 2, function(x) length(which(is.na(x)))))
+nas <- cbind(rownames(tmp),tmp);
+rownames(nas)<-NULL;colnames(nas)<-c("Neib","Nas")
+nas %>% top_n(1)
+#Q3
 
+
+#Q4
+
+mod_ds<-ames_train %>% select(price,Lot.Area,Land.Slope,Year.Built,Year.Remod.Add,Bedroom.AbvGr)
+
+#Q4
 
 
 
