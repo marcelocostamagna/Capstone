88f1763015b0e7759338b4d36e56429ccb822c5a
marcelotCasa <marcelotto696@yahoo.com.ar>
2017-02-04 17:01:26 -0300

    almost final

--- EDA-ModelSelection.R
+++ EDA-ModelSelection.R
@@ -59,9 +59,10 @@ nas %>% top_n(1)
 
 
 #Q4
-
+library(dplyr)
 library(GGally)
 library(BAS)
+install.packages(BAS)
 mod_ds<-ames_train %>% select(price,Lot.Area,Land.Slope,Year.Built,Year.Remod.Add,Bedroom.AbvGr)
 ggpairs(mod_ds)
 
@@ -124,6 +125,37 @@ bma_mod_zel_log <- bas.lm(log(price) ~ log(Lot.Area)+Land.Slope+Year.Built+Year.
 
 image(bma_mod_zel_log,rotate=F)
 #Q5
+
+#Q6
+
+bma_mod_zel_final <- bas.lm(log(price) ~ Lot.Area+Year.Built+Year.Remod.Add+Bedroom.AbvGr , 
+                      data=mod_ds,prior = "ZS-null",  
+                      modelprior = uniform())
+
+bma_mod_zel_log_final <- bas.lm(log(price) ~ log(Lot.Area)+Year.Built+Year.Remod.Add+Bedroom.AbvGr , 
+                      data=mod_ds,prior = "ZS-null",  
+                      modelprior = uniform())
+
+plot(bma_mod_zel_final, which=1, add.smooth=F)
+plot(bma_mod_zel_log_final, which=1, add.smooth=F)
+
+pra<-predict.bas(bma_mod_zel_final,ames_train_sub, prediction = T)
+tmp<-as.data.frame( as.vector(pra$Ypred))
+colnames(tmp) <-"pred_p"
+bc<-bind_cols(data.frame(ames_train$price),tmp)
+
+(pra$Ypred)
+
+pr<-as.list(pra$Ypred)
+
+hist(ames_train$Lot.Area)
+
+library(ggplot2)
+ames_train %>% select(Lot.Area) %>% ggplot(aes(x=Lot.Area)) +geom_histogram()
+library(dplyr)
+
+
+
 
 
 
